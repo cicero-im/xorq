1988c4aad3ed2f908737f3f35375cd9100a6f689
Daniel Mesejo <mesejoleon@gmail.com>
2025-06-02 16:53:42 +0200

    chore: add test

--- python/xorq/common/utils/tests/test_caching_utils.py
+++ python/xorq/common/utils/tests/test_caching_utils.py
@@ -1,10 +1,24 @@
+import sys
 from pathlib import Path
 
 from xorq.common.utils.caching_utils import user_cache_dir
 
 
+def tuple_contains(a, b) -> bool:
+    from difflib import SequenceMatcher
+
+    s = SequenceMatcher(None, b, a)
+    return s.find_longest_match().size == len(b)
+
+
 def test_default_caching_dir():
     actual_dir = user_cache_dir()
-    print(actual_dir)
     assert actual_dir is not None
     assert isinstance(actual_dir, (str, Path))
+
+    expected_parts = (
+        ("AppData", "Local", "xorq", "cache")
+        if sys.platform == "win32"
+        else (".cache", "xorq")
+    )
+    assert tuple_contains(Path(actual_dir).parts, expected_parts)
