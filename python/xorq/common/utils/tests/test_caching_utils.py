f8fad12ee1d9f473228a5f899f10e3b6690bf5f7
Daniel Mesejo <mesejoleon@gmail.com>
2025-06-02 16:32:13 +0200

    show path

--- python/xorq/common/utils/tests/test_caching_utils.py
+++ python/xorq/common/utils/tests/test_caching_utils.py
@@ -5,5 +5,6 @@ from xorq.common.utils.caching_utils import user_cache_dir
 
 def test_default_caching_dir():
     actual_dir = user_cache_dir()
+    print(actual_dir)
     assert actual_dir is not None
     assert isinstance(actual_dir, (str, Path))
